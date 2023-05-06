bc8fbe408a6c9a4e474a1d44160f03b49e14a544
Saakshi Jain <110716472+saakshijain2022@users.noreply.github.com>
2023-05-07 01:42:57 +0530

    Add files via upload

---
+++ sampletext.py
@@ -0,0 +1,29 @@
+import pygame
+ 
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+clock = pygame.time.Clock()
+ 
+dialogue_font = pygame.font.SysFont('arial', 15)
+name_font = pygame.font.SysFont('Helvetica', 20)
+game_over_font = pygame.font.SysFont('Verdana', 60)
+ 
+dialogue = dialogue_font.render("Dear Player,",
+                                True, (0,0,0))
+name = name_font.render("Your score is :30", True, (0,0,255))
+game_over = game_over_font.render("Game Over", True, (255,0,0))
+ 
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+     
+    screen.fill((255, 255, 255))
+     
+    screen.blit(dialogue, (40,40))
+    screen.blit(name, (40,140))
+    screen.blit(game_over, (40,240))
+     
+    pygame.display.flip()
+    clock.tick(60)
